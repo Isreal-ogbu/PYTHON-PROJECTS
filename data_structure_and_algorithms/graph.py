4dd0e21bb937e61938111f49b2818c29b2c7b931
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-08-27 19:07:01 +0100

    depth first search

--- data_structure_and_algorithms/graph.py
+++ data_structure_and_algorithms/graph.py
@@ -1,8 +1,13 @@
 def dfs(graph, start, visited=None):
+    # This checks if the visited node is empty
     if visited is None:
         visited = set()
+
+    # Add the start to the visited Node and print it
     visited.add(start)
     print(start, end=" ")
+
+    # I terare through the unvisited node and perform a reculsive call on each node till the end
     for next in graph[start] - visited:
         dfs(graph, next, visited)
     return
