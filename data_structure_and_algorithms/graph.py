e24de350e80dfa3b5899bee789ac80198bb3efe0
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-08-31 14:57:35 +0100

    Learning competive DSA

--- data_structure_and_algorithms/graph.py
+++ data_structure_and_algorithms/graph.py
@@ -38,3 +38,90 @@ if __name__ == "__main__":
     dfs(data, "A", "F")
     print('')
     bfs(data, "A")
+
+
+# Binary search Tree - max, min, add, preorder, postorder, inorder, search, height.
+
+class Tree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_val(self, data):
+        if self.data is None:
+            self.data = Tree(data)
+        if data < self.data:
+            if not self.left:
+                self.left = Tree(data)
+            return self.left.add_val(data)
+        if data > self.data:
+            if not self.right:
+                self.right = Tree(data)
+            return self.right.add_val(data)
+
+    def inorder(self):
+        res = []
+        if self.left:
+            res += self.left.inorder()
+        res.append(self.data)
+        if self.right:
+            res += self.right.inorder()
+        return res
+
+    def preorder(self):
+        res = [self.data]
+        if self.left:
+            res += self.left.preorder()
+        if self.right:
+            res += self.right.preorder()
+        return res
+    
+    def postorder(self):
+        res = []
+        if self.left:
+            res += self.left.postorder()
+        if self.right:
+            res += self.right.postorder()
+        res.append(self.data)
+        return res
+
+    def search(self, val):
+        if self.data == val:
+            return self.data
+        elif val < self.data:
+            if not self.left:
+                return
+            return self.left.search(val)
+        elif val > self.data:
+            if not self.right:
+                return
+            return self.right.search(val)
+
+    def getsize(self):
+        if self.left is not None and self.right is not None:
+            return 1 + self.left.getsize() + self.right.getsize()
+        elif self.right:
+            return 1 + self.right.getsize()
+        elif self.left:
+            return 1 + self.left.getsize()
+        else:
+            return 1
+
+    def min_value(self):
+        if self.left is None:
+            return self.data
+        return self.left.min_value()
+
+    def max_value(self):
+        if self.right is None:
+            return self.data
+        return self.right.max_value()
+
+
+def helper(nums):
+    test = Tree(nums[0])
+    for i in range(1, len(nums)):
+        test.add_val(nums[i])
+    return test
+    
